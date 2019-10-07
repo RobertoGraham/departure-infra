9cf01a311fde6199eeb1207f70ff523886a9bd4e
Roberto Graham <robertogrhm@gmail.com>
2019-10-07 17:30:26 +0100

    Testing paramterized source url

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "heroku_build" "departure-app" {
   app = heroku_app.departure-app.name
   source = {
     version = var.departure_app_commit_hash
-    path = "./departure-app-${var.departure_app_commit_hash}.tar.gz"
+    url = "https://github.com/RobertoGraham/departure-app/archive/${var.departure_app_commit_hash}.tar.gz"
   }
 }
 
