99a44066542972d99c47f1d304b5a2253a793309
Roberto Graham <robertogrhm@gmail.com>
2020-04-23 21:16:27 +0100

    Turn ACM on for correct app

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "heroku_app" "departure-app" {
   name   = var.departure_app_name
   region = "eu"
   stack  = "container"
+  acm    = true
   config_vars = {
     DEPARTURE_API_URL = "https://${var.departure_api_name}.herokuapp.com"
   }
@@ -53,7 +54,6 @@ resource "heroku_app" "departure-api" {
   name   = var.departure_api_name
   region = "eu"
   stack  = "container"
-  acm    = true
   sensitive_config_vars = {
     TRANSPORT_API_CLIENT_APPLICATION_KEY = var.transport_api_app_key
     TRANSPORT_API_CLIENT_APPLICATION_ID  = var.transport_api_app_id
