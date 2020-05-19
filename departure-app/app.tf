7e3fea854f103b5368a3417815f9f8290d9b8402
Roberto Graham <robertogrhm@gmail.com>
2020-05-19 22:11:43 +0100

    Temporarily comment out ACm

--- departure-app/app.tf
+++ departure-app/app.tf
@@ -2,7 +2,7 @@ resource "heroku_app" "departure-app" {
   name   = var.departure_app_name
   region = "eu"
   stack  = "container"
-  acm    = true
+//  acm    = true
   config_vars = {
     DEPARTURE_API_URL = data.terraform_remote_state.departure_api.outputs.departure_api_url
   }
