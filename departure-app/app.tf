4001e05c8bfce8aa46faf760b3026bd241bd0845
Roberto Graham <robertogrhm@gmail.com>
2020-05-19 22:19:42 +0100

    Fix API url

--- departure-app/app.tf
+++ departure-app/app.tf
@@ -2,8 +2,8 @@ resource "heroku_app" "departure-app" {
   name   = var.departure_app_name
   region = "eu"
   stack  = "container"
-//  acm    = true
+  acm    = true
   config_vars = {
-    DEPARTURE_API_URL = data.terraform_remote_state.departure_api.outputs.departure_api_url
+    DEPARTURE_API_URL = trimsuffix(data.terraform_remote_state.departure_api.outputs.departure_api_url, "/")
   }
 }
