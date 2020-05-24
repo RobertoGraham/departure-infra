d41481df88948807d23f35577d86a1486fdb9d96
Roberto Graham <robertogrhm@gmail.com>
2020-05-24 02:29:53 +0100

    Outputs fix

--- applications/departure-app/main.tf
+++ applications/departure-app/main.tf
@@ -8,13 +8,8 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "departure_api" {
-  backend   = "pg"
-  workspace = "default"
-  config = {
-    schema_name = "departure_api_terraform_remote_state"
-    conn_str    = var.database_url
-  }
+data "heroku_app" "departure-api" {
+  name = "departure-api"
 }
 
 module "application" {
@@ -22,7 +17,7 @@ module "application" {
   application_name = "departure-app"
   custom_domain    = var.departure_app_domain
   configuration_variables = {
-    DEPARTURE_API_URL = trimsuffix(data.terraform_remote_state.departure_api.outputs.url, "/")
+    DEPARTURE_API_URL = trimsuffix(data.heroku_app.departure-api.web_url, "/")
   }
   commit_hash = var.departure_app_commit_hash
 }
--- modules/application/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "url" {
-  value       = heroku_app.application.web_url
-  description = "URL of the Heroku app"
-}
