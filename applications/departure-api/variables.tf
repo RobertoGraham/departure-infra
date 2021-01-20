1c4679cb15d807818fc0f5aaedb2eeff790a34e9
Roberto Graham <robertogrhm@gmail.com>
2021-01-20 17:00:24 +0000

    Set sensitive attribute to true for sensitive variables

--- applications/departure-api/variables.tf
+++ applications/departure-api/variables.tf
@@ -1,11 +1,13 @@
 variable "transport_api_app_id" {
   type        = string
   description = "Transport API Application ID"
+  sensitive   = true
 }
 
 variable "transport_api_app_key" {
   type        = string
   description = "Transport API Application Key"
+  sensitive   = true
 }
 
 variable "departure_api_commit_hash" {
--- applications/departure-app/variables.tf
+++ applications/departure-app/variables.tf
@@ -1,6 +1,7 @@
 variable "departure_app_domain" {
   type        = string
   description = "Domain to accept traffic from"
+  sensitive   = true
 }
 
 variable "departure_app_commit_hash" {
